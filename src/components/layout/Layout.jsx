5255aa08765434d9cda9ed9ab8636ae7d889f763
Aakash Kumar <104674258+Aakashakkyji@users.noreply.github.com>
2024-08-28 17:59:19 +0530

    Update Layout.jsx

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Navbar from '../navbar/Navbar.jsx';
-import Footer from '../footer/Footer.jsx'
+import Footer from '../footer/Footer.jsx';
 
 function Layout({children}) {
   return (
